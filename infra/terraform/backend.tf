89bef01d562ef30417e491682a7b52e44f966273
Jordan Roessle <jordanroessle@gmail.com>
2026-05-18 21:44:51 -0700

    feat: terraform applied

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "fido-foster-twilio-tf-state"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "fido-foster-twilio-tf-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
